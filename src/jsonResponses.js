86931b6bb8f2a54b7b4f1bee5fc7d3785f42a758
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2021-09-20 21:56:33 -0400

    Phase 2 Update

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,12 +1,11 @@
 
-const getRandomNumber = (max=1) => {
+const getRandomJoke = (max=10) => {
     max = Number(max);
-    max = !max ? 1 : max;
-    max = max < 1 ? 1 : max;
-
-    const number = Math.random() * max;
+    max = !max ? 10 : max;
+    max = max < 10 ? 10 : max;
 
     const jokes = {
+        
     "q":"What do you call a very small valentine?","a":"A valen-tiny!",
     "q":"What did the dog say when he rubbed his tail on the sandpaper?","a":"Ruff, Ruff!",
     "q":"Why don't sharks like to eat clowns?","a":"Because they taste funny!",
@@ -23,23 +22,83 @@ const getRandomNumber = (max=1) => {
     "q":"Dad, can you put the cat out?","a":"I didnt know it was on fire.",
     "q":"What did the ocean say to the sailboat?","a":"Nothing, it just waved.",
     "q":"What do you get when you cross a snowman with a vampire?","a":"Frostbite",
+    
+    
     };
 
-    const randomize = Math.floor(number * jokes)
     
   
+    const jokeArray = Array(jokes);
+    
+    
+    
+    const random = Math.floor(Math.random() * jokeArray.length);
+    
+    const randomJoke = jokeArray[random];
 
-    return JSON.stringify(randomize);
+  
+    console.log(jokeArray);
+    return JSON.stringify(randomJoke);
+}
+
+const getRandomJokes = (max=10) => {
+    max = Number(max);
+    max = !max ? 10 : max;
+    max = max < 10 ? 10 : max;
+
+    const jokes = {
+        
+    "q":"What do you call a very small valentine?","a":"A valen-tiny!",
+    "q":"What did the dog say when he rubbed his tail on the sandpaper?","a":"Ruff, Ruff!",
+    "q":"Why don't sharks like to eat clowns?","a":"Because they taste funny!",
+    "q":"What did the boy cat say to the girl cat?","a":"You're Purr-fect!",
+    "q":"What is a frog's favorite outdoor sport?","a":"Fly Fishing!",
+    "q":"I hate jokes about German sausages.","a":"Theyre the wurst.",
+    "q":"Did you hear about the cheese factory that exploded in France?","a":"There was nothing left but de Brie.",
+    "q":"Our wedding was so beautiful ","a":"Even the cake was in tiers.",
+    "q":"Is this pool safe for diving?","a":"It deep ends.",
+    "q":"Dad, can you put my shoes on?","a":"I dont think theyll fit me.",
+    "q":"Can February March?","a":"No, but April May",
+    "q":"What lies at the bottom of the ocean and twitches?","a":"A nervous wreck.",
+    "q":"Im reading a book on the history of glue.","a":"I just cant seem to put it down.",
+    "q":"Dad, can you put the cat out?","a":"I didnt know it was on fire.",
+    "q":"What did the ocean say to the sailboat?","a":"Nothing, it just waved.",
+    "q":"What do you get when you cross a snowman with a vampire?","a":"Frostbite",
+    
+    
+    };
+
+    
+  
+    const jokeArray = Array(jokes);
+    
+    
+    
+    const random = Math.floor(Math.random() * jokeArray.length);
+    
+    const randomJoke = jokeArray[random];
+
+  
+    console.log(jokeArray);
+    return JSON.stringify(jokeArray);
 }
 
 
 
 const getRandomJokeResponse = (request, response) => {  
     response.writeHead(200, {'Content-Type': 'application/json' });
-    response.write(getRandomNumber());
+    response.write(getRandomJoke());
     response.end();
 
   
 }; 
 
+const getRandomJokesResponse = (request, response, params) =>{
+    response.writeHead(200, { 'Content-Type': 'application/json'})
+    response.write(getRandomJokes(params.limit));
+    response.end();
+
+}
+
 module.exports.getRandomJokeResponse = getRandomJokeResponse;
+module.exports.getRandomJokesResponse = getRandomJokesResponse;
